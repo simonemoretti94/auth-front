3e2902444fc9cdd4c5fc37c4678b8b73e6bf355d
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-07 15:14:00 +0200

    1.3th added modal

--- src/App.vue
+++ src/App.vue
@@ -78,13 +78,53 @@ export default {
               <img :src="base_api_url + '/storage/' + project.cover_image" :alt="project.title" style="height: 80%;">
             </template>
             <template v-else>
-              <img :src="project.cover_image" :alt="project.title" style="height: 80%;">
+              <img :src="project.cover_image" :alt="project.title" style="height: 65%;">
             </template>
 
-            <div class="card-body">
+            <div class="card-body" style="height: 25%;">
               <p><b>{{ project.title }}</b><br>
                 {{ project.description }}
               </p>
+            </div>
+
+            <div class="card-footer" style="height: 10%;">
+
+              <!-- Modal trigger button -->
+              <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
+                :data-bs-target="`#project-${project.id}`">
+                View
+              </button>
+
+              <div class="modal fade" :id="`project-${project.id}`" tabindex="-1" data-bs-backdrop="static"
+                data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-md" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" :id="`modal-title-${project.id}`">
+                        {{ project.title }}
+                      </h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body" style="overflow-y: scroll;">
+
+                      <img v-if="!project.cover_image.startsWith('https://')" class="img-fluid"
+                        :src="base_api_url + '/storage/' + project.cover_image" :alt="project.title"
+                        style="height: 80%;">
+                      <img v-else :src="project.cover_image" class="img-fluid" :alt="project.title"
+                        style="height: 80%;">
+                      <p>
+                        {{ project.description }}
+                      </p>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                        Close
+                      </button>
+                      <!-- <button type="button" class="btn btn-primary">Save</button> -->
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -92,7 +132,8 @@ export default {
       <nav class="my-3" aria-label="Page navigation">
         <ul class="pagination    ">
           <li class="page-item" :class="{ 'd-none': !link.url, 'active': link.active }" v-for="link in projects.links">
-            <button class="page-link" :href="link.url" type="button" @click="goTo(link.url)">{{ link.label }}</button>
+            <button class="page-link" :href="link.url" type="button" @click="goTo(link.url)"><span
+                v-html="link.label"></span></button>
           </li>
         </ul>
       </nav>
